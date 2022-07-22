33c30f48b43bd7179096fe3f74759764f666755e
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-07-22 16:51:47 -0300

    route

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/auth/login',[LoginController::class,"login"])->name("auth.login");
+Route::post('/login',[LoginController::class,"login"])->name("auth.login");
+
 Route::get("/login",[LoginController::class,"showLoginForm"])->name("login");
 Route::get("/login",[LoginController::class,"showLoginForm"])->name("login.view");
+
+
 Route::get("/auth/logout",[LoginController::class,"logout"])->name("auth.logout");
 
 
